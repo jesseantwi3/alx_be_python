6e152869ec63d237df3f08c1b234f06582311d1f
jesseantwi <jesseantwi3@gmail.com>
2025-08-10 17:16:05 +0000

    new code

---
+++ bank_account.py
@@ -0,0 +1,21 @@
+class BankAccount:
+    def __init__(self,account_balance=0):
+        self.account_balance = account_balance
+    def deposit(self, amount):
+        if amount > 0:
+            self.account_balance += amount
+            return True
+        else:
+            return False
+    def withdraw(self, amount):
+        if 0 < amount <= self.account_balance:
+            self.account_balance -= amount
+            return True
+        else:
+            return False        
+        
+    def account_balance(self):
+        return self.account_balance 
+    def __str__(self):
+        return f"Account balance: {self.account_balance}"     
+    
